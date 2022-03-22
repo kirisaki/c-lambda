629f3c1a61a865e496e5c88d1308c5040f3b21c5
Akihito Kirisaki <kirisaki@klara.works>
2022-03-22 20:00:43 +0900

    feat: define structs

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ exec: main.o
 	gcc -Wall -O2 -o lambda main.o
 
 main.o: main.c
-	gcc -c main.c
+	gcc -std=c99 -c main.c
--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
 
+typedef enum {
+    Var,
+    Lam,
+    App
+} expr_type;
+
+typedef struct expr {
+    expr_type type;
+    union payload{
+        struct {
+            char symbol;
+        };
+        struct {
+            char variable;
+            struct expr *expr;
+        };
+        struct {
+            struct expr *applyer;
+            struct expr *aplyee;
+        };
+    };
+} expr_t;
+
+
+
 int main(){
-    printf("nyaan\n");
+    printf("%d\n", sizeof(expr_t));
 }
