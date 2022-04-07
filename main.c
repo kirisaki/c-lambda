09f7d0423140dd82d8ae12766ade5d6e98de1beb
Akihito Kirisaki <kirisaki@klara.works>
2022-04-07 16:00:40 +0900

    feat: show expr

--- main.c
+++ main.c
@@ -26,12 +26,14 @@ typedef struct expr_t {
 
 expr *var(char c){
     expr *e = malloc(sizeof(expr));
+    e->type = Var;
     e->payload.symbol = c;
     return e;
 }
 
 expr *lam(char c, expr *e){
     expr *e0 = malloc(sizeof(expr));
+    e0->type = Lam;
     e0->payload.variable = c;
     e0->payload.expr = e;
     return e0;
@@ -39,11 +41,36 @@ expr *lam(char c, expr *e){
 
 expr *app(expr *er, expr *ee){
     expr *e = malloc(sizeof(expr));
+    e->type = App;
     e->payload.applyer = er;
     e->payload.applyee = ee;
     return e;
 }
 
+void show(expr *e){
+    switch (e->type)
+    {
+    case Var:
+        printf("(Var '%c')", e->payload.symbol);
+        break;
+    case Lam:
+        printf("(Lam '%c' ", e->payload.variable);
+        show(e->payload.expr);
+        printf(")");
+        break;
+    case App:
+        printf("(App ");
+        show(e->payload.applyer);
+        printf(" ");
+        show(e->payload.applyee);
+        printf(")");
+        break;
+    default:
+        break;
+    }
+}
+
 int main(){
-    printf("%ld\n", sizeof(expr));
+    show(app(lam('x', var('x')), var('y')));
+    printf("\n");
 }
