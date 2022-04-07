21736b4628270545e7598969aa7cb0c70a83c140
Akihito Kirisaki <kirisaki@klara.works>
2022-04-07 15:45:12 +0900

    feat: make builders

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef enum {
     Var,
@@ -6,25 +7,43 @@ typedef enum {
     App
 } expr_type;
 
-typedef struct expr {
+typedef struct expr_t {
     expr_type type;
-    union payload{
+    union payload_t{
         struct {
             char symbol;
         };
         struct {
             char variable;
-            struct expr *expr;
+            struct expr_t *expr;
         };
         struct {
-            struct expr *applyer;
-            struct expr *aplyee;
+            struct expr_t *applyer;
+            struct expr_t *applyee;
         };
-    };
-} expr_t;
+    } payload;
+} expr;
 
+expr *var(char c){
+    expr *e = malloc(sizeof(expr));
+    e->payload.symbol = c;
+    return e;
+}
 
+expr *lam(char c, expr *e){
+    expr *e0 = malloc(sizeof(expr));
+    e0->payload.variable = c;
+    e0->payload.expr = e;
+    return e0;
+}
+
+expr *app(expr *er, expr *ee){
+    expr *e = malloc(sizeof(expr));
+    e->payload.applyer = er;
+    e->payload.applyee = ee;
+    return e;
+}
 
 int main(){
-    printf("%d\n", sizeof(expr_t));
+    printf("%ld\n", sizeof(expr));
 }
